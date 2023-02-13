6591f796a2fd40f2621634b2e6df4b75332e9ef1
maskedball <maskedballmail@gmail.com>
2023-02-13 14:34:31 +0300

    add security validation

--- SgptBot/Program.cs
+++ SgptBot/Program.cs
@@ -18,39 +18,50 @@ internal static class Program
         };
         keyOption.AddAlias("-k");
         
-        Option<string> pathToSgpt = new("--path", "Path to sgpt exec")
+        Option<string> pathOption = new("--path", "Path to sgpt exec")
         {
             IsRequired = true
         };
-        pathToSgpt.AddAlias("-p");
-        
+        pathOption.AddAlias("-p");
+
+        Option<List<long>> idsOption = new(
+            "--ids",
+            "A list of allowed ids")
+        {
+            AllowMultipleArgumentsPerToken = true,
+            IsRequired = false
+        };
+
         RootCommand rootCommand = new("Telegram interface for SGPT");
         rootCommand.AddOption(keyOption);
-        rootCommand.AddOption(pathToSgpt);
+        rootCommand.AddOption(pathOption);
+        rootCommand.AddOption(idsOption);
 
-        rootCommand.SetHandler(RunCommand, keyOption, pathToSgpt);
+        rootCommand.SetHandler(RunCommand, keyOption, pathOption, idsOption);
             
         // Parse the command line arguments
         rootCommand.Invoke(args);
     }
 
-    private static void RunCommand(string key, string path)
+    private static void RunCommand(string key, string path, List<long> ids)
     {
-        Bot _ = new(key, path);
+        Bot _ = new(key, path, ids);
     }
 }
 internal class Bot
     {
         private static ILogger _logger = Log.Logger;
         private string Path { get; }
+        private List<long> Ids { get; }
 
-        public Bot(string key, string path)
+        public Bot(string key, string path, List<long> ids)
         {
             _logger = new LoggerConfiguration()
                 .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
                 .CreateLogger();
 
             Path = path;
+            Ids = ids;
             TelegramBotClient client = new(key);
             client.StartReceiving(UpdateHandler, PollingErrorHandler);
             
@@ -61,6 +72,7 @@ internal class Bot
             _logger.Error("Error:{Error}", exception.Message);
             return Task.CompletedTask;
         }
+
         private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
         {
             try
@@ -70,7 +82,7 @@ internal class Bot
                 {
                     return;
                 }
-        
+
                 if (message.Type != MessageType.Text) // working only with text
                 {
                     return;
@@ -78,7 +90,16 @@ internal class Bot
 
                 long chatId = message.Chat.Id; // user ID
                 _logger.Information("ID: {Id}, Name: {Name}", chatId, message.Chat.Username);
-                
+
+                if (Ids.Count != 0 && Ids.Contains(chatId) == false)
+                {
+                    await client.SendTextMessageAsync(chatId, "You don't have privileges to use this bot.",
+                        cancellationToken: token);
+                    _logger.Error("User {Name} with ID {Id} don't have privileges to use this bot",
+                        message.Chat.Username, chatId);
+                    return;
+                }
+
                 string? text = message.Text;
                 if (String.IsNullOrEmpty(text))
                 {
@@ -86,18 +107,19 @@ internal class Bot
                 }
 
                 string cleanedText = text.Replace("\"", "").Replace("\'", "");
-            
+
                 BufferedCommandResult result = await Cli.Wrap(Path)
                     .WithArguments($"--no-animation --no-spinner \"{cleanedText}\"")
                     .WithValidation(CommandResultValidation.None)
                     .ExecuteBufferedAsync();
                 if (result.ExitCode != 0)
                 {
-                    await client.SendTextMessageAsync(chatId, "Error. Try to paraphrase your request.", cancellationToken: token);
+                    await client.SendTextMessageAsync(chatId, "Error. Try to paraphrase your request.",
+                        cancellationToken: token);
                     _logger.Error("Exit code != 0. Error:{Error}", result.StandardError);
                     return;
                 }
-            
+
                 await client.SendTextMessageAsync(chatId, result.StandardOutput, cancellationToken: token);
             }
             catch (Exception exception)
