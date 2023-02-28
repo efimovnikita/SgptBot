e9afb14f0f93d8995de337b85519a0b80e514852
maskedball <maskedballmail@gmail.com>
2023-02-28 12:59:06 +0300

    fix logic

--- SnippetExtension/content.js
+++ SnippetExtension/content.js
@@ -5,26 +5,12 @@ button1.style.bottom = "204px";
 
 document.body.appendChild(button1);
 
-const textArea = document.querySelector(
-  "#__next > div.overflow-hidden.w-full.h-full.relative > div.flex.h-full.flex-1.flex-col.md\\:pl-\\[260px\\] > main > div.absolute.bottom-0.left-0.w-full.border-t.md\\:border-t-0.dark\\:border-white\\/20.md\\:border-transparent.md\\:dark\\:border-transparent.md\\:bg-vert-light-gradient.bg-white.dark\\:bg-gray-800.md\\:\\!bg-transparent.dark\\:md\\:bg-vert-dark-gradient > form > div > div.flex.flex-col.w-full.py-2.flex-grow.md\\:py-3.md\\:pl-4.relative.border.border-black\\/10.bg-white.dark\\:border-gray-900\\/50.dark\\:text-white.dark\\:bg-gray-700.rounded-md.shadow-\\[0_0_10px_rgba\\(0\\,0\\,0\\,0\\.10\\)\\].dark\\:shadow-\\[0_0_15px_rgba\\(0\\,0\\,0\\,0\\.10\\)\\] > textarea"
-);
-
-button1.addEventListener("click", () => {
-  textArea.value += "Rewrite this in more simple words:\n\n";
-  setHeightAndFocus();
-});
-
 const button2 = document.createElement("button");
 button2.innerHTML = "A1";
 button2.classList.add("my-button-class");
 button2.style.bottom = "144px";
 
 document.body.appendChild(button2);
-
-button2.addEventListener("click", () => {
-  textArea.value += "Rewrite this and use A1 English vocabulary:\n\n";
-  setHeightAndFocus();
-});
 
 const button3 = document.createElement("button");
 button3.innerHTML = "A2";
@@ -33,11 +19,6 @@ button3.style.bottom = "84px";
 
 document.body.appendChild(button3);
 
-button3.addEventListener("click", () => {
-  textArea.value += "Rewrite this and use A2 English vocabulary:\n\n";
-  setHeightAndFocus();
-});
-
 const button4 = document.createElement("button");
 button4.innerHTML = "B1";
 button4.classList.add("my-button-class");
@@ -45,12 +26,25 @@ button4.style.bottom = "24px";
 
 document.body.appendChild(button4);
 
-button4.addEventListener("click", () => {
-  textArea.value += "Rewrite this and use B1 English vocabulary:\n\n";
-  setHeightAndFocus();
+function setTextToClipboard(text) {
+  navigator.clipboard.readText().then((clipText) => {
+    const newText = text + clipText;
+    navigator.clipboard.writeText(newText);
+  });
+}
+
+button1.addEventListener("click", () => {
+  setTextToClipboard("Rewrite this in more simple words:\n\n");
 });
 
-function setHeightAndFocus() {
-  textArea.style.height = "200px";
-  textArea.focus();
-}
+button2.addEventListener("click", () => {
+  setTextToClipboard("Rewrite this and use A1 English vocabulary:\n\n");
+});
+
+button3.addEventListener("click", () => {
+  setTextToClipboard("Rewrite this and use A2 English vocabulary:\n\n");
+});
+
+button4.addEventListener("click", () => {
+  setTextToClipboard("Rewrite this and use B1 English vocabulary:\n\n");
+});
