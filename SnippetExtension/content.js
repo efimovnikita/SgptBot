d4b30017a5bcfef3942d1793b036a2ede68347ae
maskedball <maskedballmail@gmail.com>
2023-03-02 13:52:58 +0300

    remove redundant buttons

--- SnippetExtension/content.js
+++ SnippetExtension/content.js
@@ -1,30 +1,18 @@
 const button1 = document.createElement("button");
-button1.innerHTML = "S";
+button1.innerHTML = "AC";
 button1.classList.add("my-button-class");
-button1.style.bottom = "204px";
+button1.style.bottom = "84px";
+button1.title = "Append clipboard";
 
 document.body.appendChild(button1);
 
 const button2 = document.createElement("button");
-button2.innerHTML = "A1";
+button2.innerHTML = "CP";
 button2.classList.add("my-button-class");
-button2.style.bottom = "144px";
+button2.style.bottom = "24px";
+button2.title = "Copy all answers to clipboard";
 
 document.body.appendChild(button2);
-
-const button3 = document.createElement("button");
-button3.innerHTML = "A2";
-button3.classList.add("my-button-class");
-button3.style.bottom = "84px";
-
-document.body.appendChild(button3);
-
-const button4 = document.createElement("button");
-button4.innerHTML = "B1";
-button4.classList.add("my-button-class");
-button4.style.bottom = "24px";
-
-document.body.appendChild(button4);
 
 function setTextToClipboard(text) {
   navigator.clipboard.readText().then((clipText) => {
@@ -38,13 +26,17 @@ button1.addEventListener("click", () => {
 });
 
 button2.addEventListener("click", () => {
-  setTextToClipboard("Rewrite this and use A1 English vocabulary:\n\n");
-});
-
-button3.addEventListener("click", () => {
-  setTextToClipboard("Rewrite this and use A2 English vocabulary:\n\n");
-});
-
-button4.addEventListener("click", () => {
-  setTextToClipboard("Rewrite this and use B1 English vocabulary:\n\n");
+  const elements = document.querySelectorAll(
+    ".markdown.prose.w-full.break-words.dark\\:prose-invert.dark"
+  );
+  let text = "";
+  for (let i = 0; i < elements.length; i++) {
+    const children = elements[i].querySelectorAll("*");
+    for (let j = 0; j < children.length; j++) {
+      text += children[j].textContent.trim() + "\n\n";
+    }
+  }
+  navigator.clipboard.readText().then((clipText) => {
+    navigator.clipboard.writeText(text);
+  });
 });
