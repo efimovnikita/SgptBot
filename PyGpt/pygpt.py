0fcd21f1678b430b0b0cd1e94791ac27cc8d53a5
maskedball <maskedballmail@gmail.com>
2023-03-03 15:07:32 +0300

    add validation

--- PyGpt/pygpt.py
+++ PyGpt/pygpt.py
@@ -1,10 +1,18 @@
 import argparse
 import openai
 
+# Define command line arguments
 parser = argparse.ArgumentParser(description='Tool for communicating with GPT-3')
 parser.add_argument('--prompt', type=str, help='Prompt for GPT-3.5')
 parser.add_argument('--key', type=str, help='API KEY')
 args = parser.parse_args()
+
+# Validate arguments
+if not args.prompt:
+    parser.error("Please provide a prompt")
+
+if not args.key:
+    parser.error("Please provide an API key")
 
 openai.api_key = args.key
 
