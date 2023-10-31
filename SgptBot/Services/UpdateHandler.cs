b7b6045f3740d1241dc363ebf45c218e57910ebb
maskedball <maskedballmail@gmail.com>
2023-10-31 15:54:49 +0300

    Add reset context command

--- SgptBot/Services/UpdateHandler.cs
+++ SgptBot/Services/UpdateHandler.cs
@@ -62,6 +62,23 @@ public class UpdateHandler : IUpdateHandler
         _logger.LogInformation("The message was sent with id: {SentMessageId}", sentMessage.MessageId);
     }
 
+    private async Task<Message> ResetContextCommand(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+    {
+        var storeUser = GetStoreUser(message);
+        if (storeUser == null)
+        {
+            return await botClient.SendTextMessageAsync(message.Chat.Id, "Error getting the user from the store.",
+                cancellationToken: cancellationToken);
+        }
+        
+        RemoveAllSystemMessages(storeUser);
+        _userRepository.UpdateUser(storeUser);
+
+        return await botClient.SendTextMessageAsync(message.Chat.Id,
+            "Context prompt was deleted.",
+            cancellationToken: cancellationToken);
+    }
+
     private async Task<Message> ContextCommand(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
     {
         var storeUser = GetStoreUser(message);
@@ -87,17 +104,7 @@ public class UpdateHandler : IUpdateHandler
                 cancellationToken: cancellationToken);
         }
 
-        var systemMessages = storeUser.Conversation.Where(msg => msg.Role == Role.System).ToArray();
-        foreach (var systemMessage in systemMessages)
-        {
-            var removeStatus = storeUser.Conversation.Remove(systemMessage);
-            if (removeStatus == false)
-            {
-                return await botClient.SendTextMessageAsync(message.Chat.Id,
-                    "Error while removing the system conversation message.",
-                    cancellationToken: cancellationToken);
-            }
-        }
+        RemoveAllSystemMessages(storeUser);
 
         var newSystemMessage = new SgptBot.Models.Message(Role.System, contextPrompt);
         storeUser.Conversation.Insert(0, newSystemMessage);
@@ -108,6 +115,15 @@ public class UpdateHandler : IUpdateHandler
             message.Chat.Id, 
             "Context prompt was set.",
             cancellationToken: cancellationToken);
+    }
+
+    private static void RemoveAllSystemMessages(StoreUser storeUser)
+    {
+        var systemMessages = storeUser.Conversation.Where(msg => msg.Role == Role.System).ToArray();
+        foreach (var systemMessage in systemMessages)
+        {
+            storeUser.Conversation.Remove(systemMessage);
+        }
     }
 
     private async Task<Message> ModelCommand(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
