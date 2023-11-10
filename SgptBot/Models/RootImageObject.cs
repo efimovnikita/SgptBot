69e5fd2f55142867a75c6ad3558ca71d7695a80d
maskedball <maskedballmail@gmail.com>
2023-11-10 21:14:37 +0300

    Remove unused classes

--- SgptBot/Models/RootImageObject.cs
+++
@@ -1,8 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace SgptBot.Models;
-
-public record RootImageObject(
-    [property: JsonPropertyName("created")] int Created,
-    [property: JsonPropertyName("data")] IReadOnlyList<ImageObjectData> Data
-);
