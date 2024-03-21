8dcb5e83cacbd4b3f574c7853aec2fa959eba708
Nikita Efimov <maskedballmail@gmail.com>
2024-03-21 11:58:49 +0300

    Added qstar logic

--- QStarConsole/Program.cs
+++ QStarConsole/Program.cs
@@ -1,12 +1,19 @@
-﻿namespace QStarConsole;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OpenAiNg;
+using OpenAiNg.Chat;
+using OpenAiNg.ChatFunctions;
+using OpenAiNg.Models;
+
+namespace QStarConsole;
 
 internal class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         const string variableName = "OPENAI_API_KEY";
-        string? keyEnvVar = Environment.GetEnvironmentVariable(variableName);
-        if (String.IsNullOrEmpty(keyEnvVar))
+        string? key = Environment.GetEnvironmentVariable(variableName);
+        if (String.IsNullOrEmpty(key))
         {
             Console.WriteLine($"I need the env variable '{variableName}'.");
             return;
@@ -20,6 +27,145 @@ internal class Program
             return;
         }
         
+        OpenAiApi api = new(key);
         
+        ChatMessage[] chatMessages =
+        [
+            new ChatMessage(ChatMessageRole.User, prompt)
+        ];
+        
+        ChatRequest request = new()
+        {
+            Model = Model.GPT4_1106_Preview,
+            Messages = chatMessages,
+            Temperature = 0.9,
+            NumChoicesPerMessage = 5
+        };
+
+        var result = await api.Chat.CreateChatCompletionAsync(request);
+        string[] choices = (result.Choices ?? throw new InvalidOperationException()).Select(choice =>
+                choice.Message != null
+                    ? choice.Message.Content ?? ""
+                    : "")
+            .ToArray();
+
+
+        string functionPrompt = $"""
+                    Get only the number of the most accurate and precise answer to the user's question (in your opinion) in json format.
+                    
+                    USER QUESTION:
+                    
+                    [{prompt}]
+                    
+                    ANSWER N 1:
+                    
+                    [{choices[1]}]
+                    
+                    ANSWER N 2:
+                    
+                    [{choices[0]}]
+                    
+                    ANSWER N 3:
+                    
+                    [{choices[2]}]
+                    
+                    ANSWER N 4:
+                    
+                    [{choices[3]}]
+                    
+                    ANSWER N 5:
+                    
+                    [{choices[4]}]
+                    """;
+        
+        JObject jObjectDescription = new()
+        {
+            {
+                "type", "object"
+            },
+            {
+                "properties", new JObject
+                {
+                    {
+                        "number", new JObject
+                        {
+                            {"type", "integer"},
+                            {
+                                "description",
+                                "The number of the most accurate and precise answer to the user's question."
+                            }
+                        }
+                    },
+                    {
+                        "additional", new JObject
+                        {
+                            {"type", "string"},
+                            {
+                                "description",
+                                "Any additional data, that considered to be valuable."
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                "required", new JArray("number")
+            }
+        };
+        
+        ChatRequest functionRequest = new()
+        {
+            Model = Model.GPT4_1106_Preview,
+            Messages = new List<ChatMessage>(1)
+            {
+                new(ChatMessageRole.User, functionPrompt)
+            },
+            ResponseFormat = new ChatRequestResponseFormats {Type = ChatRequestResponseFormatTypes.Json},
+            Tools = new List<Tool>
+            {
+                new(new ToolFunction(name: "GetAccurateAnswer",
+                    description: "The function for extraction the number of the most accurate and precise answer to the user's question.",
+                    parameters: jObjectDescription))
+            },
+            Temperature = 0,
+            NumChoicesPerMessage = 1
+        };
+        
+        var functionCallResponse = await api.Chat.CreateChatCompletionAsync(functionRequest);
+        string functionCallArguments = functionCallResponse.Choices?[0].Message?.ToolCalls?[0].FunctionCall.Arguments ??
+                                       string.Empty;
+        if (String.IsNullOrWhiteSpace(functionCallArguments))
+        {
+            Console.WriteLine("Function arguments is empty");
+            return;
+        }
+
+        GetAccurateAnswerFunctionResult? functionResult = null;
+        try
+        {
+            functionResult = JsonConvert.DeserializeObject<GetAccurateAnswerFunctionResult>(functionCallArguments);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        
+        if (functionResult == null)
+        {
+            Console.WriteLine("Problems with deserialization");
+            return;
+        }
+
+        int number = functionResult.Number;
+
+        string mva = choices[number - 1];
+
+        Console.WriteLine($"{mva}");
     }
+}
+
+public class GetAccurateAnswerFunctionResult
+{
+    public int Number { get; set; }
+    [JsonProperty(PropertyName = "additional")] public string AdditionalInfo { get; set; } = "";
 }
