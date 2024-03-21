cd44efff9646e3b1092d1ff4d6f7ea6216422109
Nikita Efimov <maskedballmail@gmail.com>
2024-03-21 10:19:07 +0300

    Added a new project qstar

---
+++ QStarConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace QStarConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        const string variableName = "OPENAI_API_KEY";
+        string? keyEnvVar = Environment.GetEnvironmentVariable(variableName);
+        if (String.IsNullOrEmpty(keyEnvVar))
+        {
+            Console.WriteLine($"I need the env variable '{variableName}'.");
+            return;
+        }
+        
+        Console.Write("Give me your question: ");
+        string? prompt = Console.ReadLine();
+        if (String.IsNullOrEmpty(prompt))
+        {
+            Console.WriteLine("Prompt was empty.");
+            return;
+        }
+        
+        
+    }
+}
